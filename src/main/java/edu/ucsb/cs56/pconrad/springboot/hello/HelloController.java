9420675e6dd3afb74c02c6067af1940324b3c9d0
Yinon Rousso <yinon@169-231-122-191.wireless.ucsb.edu>
2018-11-27 18:59:42 -0800

    left a comment

--- src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Map;
-
+//yinon was here
 @Controller
 public class HelloController {
 	/*
