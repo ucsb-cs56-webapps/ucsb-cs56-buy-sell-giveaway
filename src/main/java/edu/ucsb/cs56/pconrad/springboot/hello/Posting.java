e20d9263b9dd6c6f0e7802c5ee493d709e5c14a8
Yinon Rousso <yinon@169-231-117-155.wireless.ucsb.edu>
2018-11-29 20:04:44 -0800

    fixed Posting.java

--- src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/Posting.java
+++ src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/Posting.java
@@ -3,14 +3,6 @@ package edu.ucsb.cs56.pconrad.springboot.hello;
 public class Posting {
 	private String title;
 	private String description;
-	private String contact;
-	private int id;
-	public Posting(String title, String description, String contact) {
-		this.title = title;
-		this.description = description;
-		this.contact = contact;
-		this.id=0;
-	}
 	private String email;
 	private String number;
 	private int approved;
@@ -42,6 +34,4 @@ public class Posting {
 	public void setApproved() {
 		this.approved = approved;
 	}
-
-	//Test
 }
